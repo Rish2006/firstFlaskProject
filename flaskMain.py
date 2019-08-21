4b4e44296cb96971311b7640d82c4215713c619f
Rishith <rishdafish1234@gmail.com>
2019-08-21 07:25:48 -0400

    Started webpage with some features

--- flaskMain.py
+++ flaskMain.py
@@ -1,13 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+
+{
+    'author': 'Rishitest',
+    'title': 'Test Blog post',
+    'content': 'test content',
+    'date_posted': 'July 14, 2019'
+}, 
+{   'author': 'Rishitest 2',
+    'title': 'Test Blog post 2',
+    'content': 'test content 2',
+    'date_posted': 'July 15, 2019'
+    }
+
+]
+
 
 @app.route("/homepage")
 @app.route("/")
 def mainPage():
-    return "<h1>Hello World!!</h1>"
+    return render_template('main.html', posts = posts)
 
 @app.route("/about")
 def aboutPage():
-    return "<h1>About Page</h1>"
+    return render_template('about.html')
 if __name__=='__main__':
     app.run(debug=True)
